361ce711380994b015f5b55b9b74df100ea07db5
Olle <me@olle.io>
2021-03-12 17:11:38 +0100

    add tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 
+import '../styles/app.css';
+
 const App = ({ Component, pageProps }: AppProps): JSX.Element => <Component {...pageProps} />;
 
 export default App;
