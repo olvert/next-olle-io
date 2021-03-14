80c4f3646fd16ef7a83fa3947bfd1ee4415ee3cd
Olle <me@olle.io>
2021-03-14 17:42:32 +0100

    add key to items

--- components/ScrobbleSection.tsx
+++ components/ScrobbleSection.tsx
@@ -6,11 +6,13 @@ type Props = {
   items: Item[];
 }
 
+const getKey = (t: Item): string => `${t.artist}+${t.name}`.split(' ').join('_').toLocaleLowerCase();
+
 const ScrobbleSection = ({ title, items }: Props): JSX.Element => (
   <div className="pt-4 pb-2">
     <h3 className="font-semibold tracking-wide">{title}</h3>
     { items.map((t) => (
-      <div className="text-sm my-2 p-2 rounded border border-gray-800 hover:bg-gray-800">
+      <div key={getKey(t)} className="text-sm my-2 p-2 rounded border border-gray-800 hover:bg-gray-800">
         <p className="font-semibold">{t.name}</p>
         <p className="text-gray-400">{t.artist}</p>
         <p className="text-gray-400">{`${t.playCount} plays`}</p>
